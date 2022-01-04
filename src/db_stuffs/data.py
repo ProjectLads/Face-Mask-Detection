d9d55020dede1a994d56a89aaaffeb9f815c0652
sb2310 <saptarshibanerjee2310@gmail.com>
2022-01-04 11:53:47 +0530

    updated image

--- src/db_stuffs/data.py
+++ src/db_stuffs/data.py
@@ -1,9 +1,9 @@
 import cv2 
 import matplotlib.pyplot as plt
-DATA_DIR = "data.csv"
-IMAGE_MASK_DIR = "D:\Projectlads\Face-Mask-Detection\src\db_stuffs\images\mask"
+DATA_DIR = "C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection/src/db_stuffs/data.csv"
+IMAGE_MASK_DIR = "C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection/src/db_stuffs/images/mask"
 
-IMAGE_UNMASK_DIR = "D:\Projectlads\Face-Mask-Detection\src\db_stuffs\images\withoutMask" 
+IMAGE_UNMASK_DIR = "C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection\src/db_stuffs/images/withoutMask" 
 MASK_DIR = "/home/rupam/Face-Mask-Detection/src/db_stuffs/images/mask"
 
 
@@ -39,15 +39,16 @@ with open(DATA_DIR) as data:
     
     info = info[1:]
     for row in info:
-      #  image.append((IMAGE_MASK_DIR + '\\' + row[0] + '.jpeg', IMAGE_UNMASK_DIR + '\\' + row[0] + '.jpeg'))
-        image.append((MASK_DIR + '/' + row[0] + '.jpeg', UNMASK_DIR + '/' + row[0] + '.jpeg'))
+        image.append((IMAGE_MASK_DIR + '\\' + row[0] + '.jpeg', IMAGE_UNMASK_DIR + '\\' + row[0] + '.jpeg'))
+        #image.append((MASK_DIR + '/' + row[0] + '.jpeg', UNMASK_DIR + '/' + row[0] + '.jpeg'))
 
     #print(image)
 
 for i in image:
     #print(i[0])
 
-    img = cv2.imread(i[1])
+    #img = cv2.imread(i[1]) #unmask run
+    img = cv2.imread(i[0]) #mask run
     img_ret = detect_face(img)
     #print(img_ret)
     
