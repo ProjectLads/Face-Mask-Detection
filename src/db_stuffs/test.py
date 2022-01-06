77b3c15192154211f44763370e8f6e54d3275365
Sandip <sandipghoshhabra@gmail.com>
2022-01-06 12:50:26 +0530

    final insertion and fetching data file

--- src/db_stuffs/test.py
+++
@@ -1,7 +0,0 @@
-DATA_DIR = "D:\Projectlads\Face-Mask-Detection\src\db_stuffs\data.csv"
-info = []
-with open(DATA_DIR) as data:
-    for row in data:
-        info.append(row.split(',')[1:6])
-    info = info[1:]
-    print(info)
