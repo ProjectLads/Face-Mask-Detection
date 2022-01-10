bcd4b772535e1be2831bc275295c766e95fcdd2c
Saptarshi Banerjee <saptarshibanerjee2310@gmail.com>
2022-01-10 22:05:27 +0530

    testing sms api, date: 10/01/2022

--- src/db_stuffs/api.py
+++ src/db_stuffs/api.py
@@ -1,16 +1,27 @@
 from PIL import Image as image 
- 
 import base64
 import requests 
 import os 
 import matplotlib.pyplot as plt
 import numpy as np
-import cv2 
+import cv2
+from requests.models import Response 
 
+def send_sms(phone_number, message):
+    url = f"https://www.fast2sms.com/dev/bulkV2?authorization=XqTFcAC4MktpQsSyKEnvPeOwjB51rhdR78YNDWVHg6bzIuo9fJNhi0OcLFJIV3sndbty42mEZ5XrHfpg&route=v3&sender_id=TXTIND&message={message}&language=english&flash=0&numbers={phone_number}"
+    resp = requests.get(url)
+    try:
+        if resp.status_code != 200:
+            print(resp.content)
+            raise Exception("Something must have went wrong with SMS Service trial. Please Check")
+    except Exception as e :
+        pass
 
 def face_match(image1 , image2 , apikey):
-    PATH1 = os.getcwd() + "/tmp/image1.jpeg"
-    PATH2 = os.getcwd() + "/tmp/image2.jpeg"
+    #PATH1 = os.getcwd() + "/tmp/image1.jpeg"
+    #PATH2 = os.getcwd() + "/tmp/image2.jpeg"
+    PATH1 = "C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection/src/db_stuffs/tmp/image1.jpeg"
+    PATH2 = "C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection/src/db_stuffs/tmp/image2.jpeg"
 
 
     image.fromarray(image1.astype(np.uint8)).save(PATH1 , quality = 100)
--- src/db_stuffs/search.py
+++ src/db_stuffs/search.py
@@ -3,16 +3,19 @@ import convert
 import matplotlib.pyplot as plt
 import cv2
 from data import detect_face
-from api import face_match
-    
+from api import face_match, send_sms
+import tensorflow as tf
+import numpy as np
+from testing_main import decode_pred
+
 query  = '''Select * from USER_IMAGE'''
 password = "XIho3BxFB7Z4u2Hj"
 username = "nlz47787"
 database = "bludb"
 hostname = '8e359033-a1c9-4643-82ef-8ac06f5107eb.bs2io90l08kqb1od8lcg.databases.appdomain.cloud'
-#path =  'C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection/src/db_stuffs/certificate.crt'
+path =  'C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection/src/db_stuffs/certificate.crt'
 
-path = '/home/rupam/Face-Mask-Detection/src/db_stuffs/certificate.crt'
+#path = '/home/rupam/Face-Mask-Detection/src/db_stuffs/certificate.crt'
 port = 30120
 uri =f"DATABASE={database};HOSTNAME={hostname};PORT={port};SECURITY=SSL;SSLServerCertificate={path};UID={username};PWD={password}" 
 
@@ -24,38 +27,61 @@ statement = ibm_db.prepare(conn1 , query)
 ibm_db.execute(statement)
 result_set = ibm_db.fetch_tuple(statement)
 
-PATH = "/home/rupam/Face-Mask-Detection/src/db_stuffs/image1.jpeg"
+#PATH = "/home/rupam/Face-Mask-Detection/src/db_stuffs/image1.jpeg"
+PATH = "C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection/src/db_stuffs/image1.jpeg"
 test_image = cv2.imread(PATH)
 test_image = cv2.resize(detect_face(test_image) , (300 , 300))
 
+test_model = tf.keras.models.load_model("C:/Users/hp/Desktop/ProjectLads/Face-Mask-Detection/src/models/model_2.h5")
 
-while result_set:
-    query  = '''Select * from USER_INFO where UID=?'''
-    user_id = result_set[3] 
-    img_arr1 = convert.text_to_image(result_set[1])
-    img_arr2 = convert.text_to_image(result_set[2])
-    apikey = "I39243ua8sbTpjaN2632"
-    
-    
-        
+final_image = cv2.resize(test_image, (224, 224))
+final_image = final_image/255.0
+#plt.imshow(final_image)
+#plt.show()
+final_image = np.expand_dims(final_image, axis = 0)
+#final_image = final_image/255.0
 
-    if face_match(test_image , img_arr1, apikey) or face_match(test_image , img_arr2 ,apikey):
-        statement1 = ibm_db.prepare(conn1 , query)
-        ibm_db.execute(statement1, (user_id, ))
-        result = ibm_db.fetch_tuple(statement1)
-        
-        print(result[0], result[1], result[2], result[3], result[4], result[5])
-        found = True
-        break
 
-        
+prediction = test_model.predict(final_image)
+prediction = decode_pred(prediction[0])
 
-    result_set = ibm_db.fetch_tuple(statement)
-    
-
-if found:
-    print("image found")
+if prediction == 0:
+    print("No mask")
 else:
-    print("image not found")
+    print("Mask")
+
+if prediction == 0:
+    while result_set:
+        query  = '''Select * from USER_INFO where UID=?'''
+        user_id = result_set[3] 
+        img_arr1 = convert.text_to_image(result_set[1])
+        img_arr2 = convert.text_to_image(result_set[2])
+        apikey = "I39243ua8sbTpjaN2632"
+    
+    
+
+
+        if face_match(test_image , img_arr1, apikey) or face_match(test_image , img_arr2 ,apikey):
+            statement1 = ibm_db.prepare(conn1 , query)
+            ibm_db.execute(statement1, (user_id, ))
+            result = ibm_db.fetch_tuple(statement1)
+        
+            #print(result[0], result[1], result[2], result[3], result[4], result[5])
+            phone_number = result[3]
+            found = True
+            break
+
+        
+
+        result_set = ibm_db.fetch_tuple(statement)
+    
+
+    if found:
+        print("image found")
+        message = '''You haven't worn a mask. Please wear a mask for everyone's safety! Join us: https://meet.google.com/wrh-kdfz-bmf 
+        With regards,ProjectLads'''
+        send_sms(phone_number, message)
+    else:
+        print("image not found")
 
 ibm_db.close(conn1)
